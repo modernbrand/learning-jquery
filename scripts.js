bba37c5db526e9ded0e2a78dbc6983e61be4471d
Bradford Kachelhofer <bradfordkachelhofer@MacBook-Air.localdomain>
2017-12-07 16:31:02 -0600

    thursday aft

---
+++ scripts.js
@@ -0,0 +1,82 @@
+// // first div
+// $(document).ready(function () {
+// var btn = document.createElement("button");
+// var text = document.createTextNode("TaDa!");
+// btn.style.color="red";
+// btn.appendChild(text);
+// document.body.appendChild(btn);
+// })
+
+
+
+
+// // second div
+// $("new-btn").click(function() {
+//     $("#results").append("arrggh");
+//     });
+
+
+
+// // third div
+// $(document).ready(function () {
+//     $("#item-three").css("background-color", "white");
+// });
+
+
+// $("#item-three").mouseover(function () {
+//     $("#item-three").css('background-color', 'blue');
+// });
+
+// $("#item-three").mouseout(function () {
+//     $("#item-three").css('background-color', 'red');
+// });
+
+
+// // fourth div difference is the ++
+// var colors = ["red", "white", "blue", "green"];
+// let x = 0;
+// $("#item-four").click(function () {
+//     $("#item-four").css("color", colors[x++]);
+// })
+
+
+// fifth div
+$("#item-five").click(function () {
+    $("#item-five")
+})
+
+
+// function addColorfulFriends() {
+//     let div = $("#jqueryDiv");
+//     let x = 0;
+//     for (let i = 0; i < friends.length; i++) {
+//         let h1 = $("<h1 class='colorName'>").append(friends[i]);
+//         div.append(h1);
+//     }
+//     let button = $("<button class='colorChanger'>").append("Change Color!");
+//     $(".buttonHolder").append(button);
+//     $(".colorChanger").click(function () {
+//         $(".colorName").css("color", colors[x])
+//         x++;
+//     })
+// };
+
+// form submissions
+// $('#btn').click(function() {
+//     let $name = $("#name").val();
+//     let $email = $("#email").val();
+//     let $message = $("#message").val();
+
+//     $http.post({
+//         url: 'https://amadeupadress.com',
+//         body = {
+//             name: $name,
+//         }
+//     })
+// })
+
+
+// empty out the Form
+// $('#name').val('');
+// $("#email").val();
+// $("#message").val();
